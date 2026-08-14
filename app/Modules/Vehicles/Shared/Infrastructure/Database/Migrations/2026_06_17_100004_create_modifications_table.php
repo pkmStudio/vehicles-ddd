3e4e9e36cac94017dd53dcea3911ac033ded5c62
Edward <pkmStudio77@gmail.com>
2026-08-14 13:47:26 +0300

    Импорт модификаций.

--- app/Modules/Vehicles/Shared/Infrastructure/Database/Migrations/2026_06_17_100004_create_modifications_table.php
+++ app/Modules/Vehicles/Shared/Infrastructure/Database/Migrations/2026_06_17_100004_create_modifications_table.php
@@ -19,19 +19,19 @@ return new class extends Migration
             $table->integer('ms_id')->comment('Внешний ID модели');
             $table->integer('mod_id')->comment('Внешний ID модификации');
 
-            $table->year('year_from')->nullable();
+            $table->year('year_from')->comment('Начало периода выпуска модификации');
             $table->year('year_to')->nullable();
 
             $table->string('localized_name')->nullable()->comment('Локализованное название');
-            $table->string('description')->nullable()->comment('Описание модификации из Текдока');
+            $table->string('description')->comment('Описание модификации из Текдока');
             $table->string('description_short')->nullable()->comment('Наше описание модификации');
             $table->string('type')->comment('VehicleTypeEnum');
 
-            $table->smallInteger('power_ps')->nullable()->comment('Мощность (л.с.)');
-            $table->smallInteger('power_kw')->nullable()->comment('Мощность (kw)');
+            $table->smallInteger('power_ps')->comment('Мощность (л.с.)');
+            $table->smallInteger('power_kw')->comment('Мощность (kw)');
 
             $table->string('brake_system_type')->nullable()->comment('BrakeSystemTypeEnum');
-            $table->string('engine_type')->nullable()->comment('EngineTypeEnum');
+            $table->string('engine_type')->comment('EngineTypeEnum');
             $table->string('gear_type')->nullable()->comment('GearTypeEnum');
             $table->string('drive_type')->nullable()->comment('DriveTypeEnum');
 
